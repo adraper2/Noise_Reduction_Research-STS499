4645e6eba39198525a1cd7b9e66c61807d287d2e
Aidan Draper <adraper2@elon.edu>
2019-02-12 10:33:20 -0500

    added SSIM benchmark

--- r_scripts/benchmarks.R
+++ r_scripts/benchmarks.R
@@ -4,6 +4,7 @@
 rm(list=ls())
 
 require(raster)
+require('SPUTNIK')
 
 getwd()
 
@@ -11,7 +12,7 @@ getwd()
 setwd('~/Documents/Senior_Year/STS 499/499_Pres')
 
 # SELECT CURRENT IMAGE HERE:
-filter.img <- as.matrix(raster("street_noise.jpg"))
+filter.img <- as.matrix(raster("street-mean_py.jpg"))
 #noise.img <- as.matrix(raster("IMG_0692.CR2"))
 true.img <- as.matrix(raster("street_true.jpg"))
 
@@ -23,3 +24,5 @@ psnr.noise <- 20*log10(255^2/MSE.noise)
 
 rsq.filter <- 1 - (sum((true.img-filter.img)^2)/sum((true.img-mean(true.img))^2))
 rsq.noise <- 1 - (sum((true.img-noise.img)^2)/sum((true.img-mean(true.img))^2))
+
+SSIM(filter.img,true.img, numBreaks = 256)
