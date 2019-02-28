3c3dfc5bc80a53fc7fa9dc7466659a368762f34e
Aidan Draper <adraper2@elon.edu>
2019-02-28 00:24:35 -0500

    added time elapsed

--- py_scripts/bilateral.py
+++ py_scripts/bilateral.py
@@ -9,21 +9,24 @@ Created on Sun Oct 21 15:12:17 2018
 #import numpy as np
 import cv2
 import os
+import time
 #import math
 
 #img_dest = '~/../../Volumes/Draper_HD/STS499_dataset/IMG_0692.CR2'
-img_dest = '499_Pres/shoes_noise.jpg'
+img_dest = '499_Pres/street_noise.jpg'
 
-os.chdir('/Users/Draper/Desktop/')
+os.chdir('/Users/Draper/Documents/Senior_Year/STS 499/')
 
 def main():
     img = cv2.imread(img_dest, 0)
     print(img)
-    #bilateral_img = cv2.bilateralFilter(img,9,100.0,100.0)
-    nonlocal_img = cv2.fastNlMeansDenoising(img, None, 20, 25,25)
+    start_time = time.time()
+    bilateral_img = cv2.bilateralFilter(img,9,100.0,100.0)
+    #nonlocal_img = cv2.fastNlMeansDenoising(img, None, 20, 25,25)
+    elapsed_time = time.time() - start_time
     #cv2.imwrite("shoes-bilateral.jpg", bilateral_img)
-    cv2.imwrite("shoes-nonlocal.jpg", nonlocal_img)
-    print('DONE!')
+    #cv2.imwrite("shoes-nonlocal.jpg", nonlocal_img)
+    print('DONE!', elapsed_time)
     
 main()
     
--- py_scripts/mean_filter.py
+++ py_scripts/mean_filter.py
@@ -8,6 +8,7 @@ Created on Tue Nov 27 20:29:14 2018
 
 import cv2
 import os
+import time
 
 img_dest = 'street_noise.jpg'
 os.chdir('/Users/Draper/Documents/Senior_Year/STS 499/499_Pres/')
@@ -15,6 +16,7 @@ os.chdir('/Users/Draper/Documents/Senior_Year/STS 499/499_Pres/')
 def main():
     img = cv2.imread(img_dest, 0)
     print(img)
+    start_time = time.time()
     #print(img.shape[0], img.shape[1])
     for x in range(img.shape[0])[1:-1]:
         for y in range(img.shape[1])[1:-1]:
@@ -22,9 +24,10 @@ def main():
                          int(img[x][y-1]) + int(img[x][y]) + int(img[x][y+1]) + 
                          int(img[x-1][y-1]) + int(img[x-1][y]) + int(img[x-1][y+1]))/9
 
+    elapsed_time = time.time() - start_time
 
 
-    cv2.imwrite("street-mean_py.jpg", img)
-    print('DONE!')
+    #cv2.imwrite("street-mean_py.jpg", img)
+    print('DONE!', elapsed_time)
     
 main()
